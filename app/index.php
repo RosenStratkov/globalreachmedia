8d5f3ac5857439aa67597503a5de685f82c724cb
RosenStratkov <32883761+RosenStratkov@users.noreply.github.com>
2026-03-30 15:50:37 +0300

    WIP remove code and test changes

--- app/index.php
+++ app/index.php
@@ -5,25 +5,13 @@ $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASSWORD');
 
-#$dsn = "mysql:dbname=$db;host=$host";
-#try {
-#    $pdo = new PDO($dsn, $user, $pass);
-#    echo "Connected to database successfully!";
-#} catch (PDOException $e) {
-#    echo "Connection failed: " . $e->getMessage();
-#}
-
-#$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
-#$pdo = new PDO($dsn, $user, $pass);
-
-
 $socket = "/cloudsql/$connectionName";
 
 try {
     $dsn = "mysql:unix_socket=$socket;dbname=$db;charset=utf8mb4";
     $pdo = new PDO($dsn, $user, $pass);
 
-    echo "Connected to database successfully!";
+    echo "Connected to database successfully!  ~~~~~~~~TEST TEST TEST~~~~~~~~~~~~~~";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
